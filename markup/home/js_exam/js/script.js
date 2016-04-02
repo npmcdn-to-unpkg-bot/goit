13a995a69eca1c6c5f7e364c7591b834b87c75dd
xlirik <xlirik@gmail.com>
2016-04-02 15:28:06 +0300

    desktop done

--- markup/home/js_exam/js/script.js
+++ markup/home/js_exam/js/script.js
@@ -6,18 +6,41 @@ $(function () {
     $('.bxslider.3').bxSlider({
         startSlide: 2
     });
+    $(".main__search-button").on("click", function () {
+        var dataObject;
+        var searchItem = $('.main__search input').val();
+        var results = document.querySelector(".main__dicsover-masonry");
+        var masonryTarget = $('.main__dicsover-masonry');
+        $.ajax({
+            url: 'http://api.pixplorer.co.uk/image?word=' + searchItem + '&amount=9&size=m',
+            dataType: "json",
+            success: function (data) {
+                dataObject = data;
+                results.innerHTML = tmpl("item_tmpl", dataObject);
+                console.log(results);
+                masonryTarget.masonry({
+                    itemSelector: '.grid-item',
+                    columnWidth: 310,
+                    percentPosition: true
+                });
+                masonryTarget.masonry('reloadItems');
+                masonryTarget.masonry('layout');
+            }
+        });
+    });
 });
 
 $(function () {
     var dataObject;
+    var masonryTarget = $('.main__dicsover-masonry');
     $.ajax({
-        url: 'http://api.pixplorer.co.uk/image?word=night&amount=9&size=tb',
+        url: 'http://api.pixplorer.co.uk/image?amount=9&size=m',
         dataType: "json",
         success: function (data) {
             dataObject = data;
             var results = document.querySelector(".main__dicsover-masonry");
-            results.innerHTML = tmpl("item_tmpl", dataObject); 
-            $('.main__dicsover-masonry').masonry({
+            results.innerHTML = tmpl("item_tmpl", dataObject);
+            masonryTarget.masonry({
                 itemSelector: '.grid-item',
                 columnWidth: 310,
                 percentPosition: true
